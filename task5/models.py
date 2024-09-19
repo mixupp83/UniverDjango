15f2d29e89600a14d128451ed205cb0c6a1da5f9
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-09-19 12:44:27 +0400

    ДЗ 19_4

--- task5/models.py
+++ task5/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=100)
